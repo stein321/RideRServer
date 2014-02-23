2f2bb654c70516b001f8c8aada88969aba694d3c
Ben Stein <stein321@gmail.com>
2014-02-23 10:17:27 -0800

    should be stable. return 1000 results

--- EventMembers.php
+++ EventMembers.php
@@ -8,15 +8,15 @@ class EventMembers{
 	private $secret_key;
 	private $app_id;
 
-	public function __construct($eid) {
+	public function __construct($event_id) {
 			$this->secret_key="f57c5b006bae8c63bc170578bd582589";
 			$this->app_id="436860186416914";
-			$this->eid=$eid;
+			$this->eid=$event_id;
 			$config=array(
 				'appId' => $this->app_id,
 			     'secret' => $this->secret_key,
 			     'file_upload' => false,
-			     'allowSignedRequest' => false,
+			     'allowSignedRequest' => true,
 			);
 			$this->facebook=new facebook($config);
 	}

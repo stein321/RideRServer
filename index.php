5dc24e43d424192a05218904aeab7d12d04b0134
Ben Stein <stein321@gmail.com>
2014-02-23 12:07:25 -0800

    changed array syntax

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 require_once('vendor/facebook/php-sdk/src/facebook.php');
 include_once('getFriends.php');
-//include_once('EventMembers.php');
-include_once('FindMutualFriends.php');
+include_once('EventMembers.php');
+//include_once('FindMutualFriends.php');
 
 $secret="f57c5b006bae8c63bc170578bd582589";
 $app_id="436860186416914";

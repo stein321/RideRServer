585f03bae49b7c4f833b12f59d19b38c8e1b75c5
Ben Stein <stein321@gmail.com>
2014-02-23 12:12:15 -0800

    changed array syntax

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 require_once('vendor/facebook/php-sdk/src/facebook.php');
-include_once('getFriends.php');
+//include_once('getFriends.php');
 include_once('EventMembers.php');
 //include_once('FindMutualFriends.php');
 
@@ -10,7 +10,7 @@ $secret="f57c5b006bae8c63bc170578bd582589";
 $app_id="436860186416914";
 
 // var_dump($_GET);
-$id=$_GET['id'];
+//$id=$_GET['id'];
 //$facebook=new Get_Friends($id);
 // $friends=$facebook->get_friends();
 //var_dump($friends);

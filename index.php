576881a61cf08da9c3a927a814d0a280a029edf2
Ben Stein <stein321@gmail.com>
2014-02-22 23:08:01 -0800

    t

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ include_once('getFriends.php');
 $secret="f57c5b006bae8c63bc170578bd582589";
 $app_id="436860186416914";
 
+var_dump($_GET);
+die();
 $id=$_GET['id'];
 $facebook=new Get_Friends($id);
 $friends=$facebook->get_friends();

475d61a0791717579523e29030c42348b167c971
Ben Stein <stein321@gmail.com>
2014-02-23 09:56:42 -0800

    changed array access for 'next' parsing

--- EventMembers.php
+++ EventMembers.php
@@ -9,28 +9,28 @@ class EventMembers{
 	private $app_id;
 
 	public function __construct($eid) {
-			$this->secret_key="f57c5b006bae8c63bc170578bd582589";
-			$this->app_id="436860186416914";
-			$this->eid=$eid;
-			$config=array(
-				'appId' => $this->app_id,
-			     'secret' => $this->secret_key,
-			     'file_upload' => false,
-			     'allowSignedRequest' => false,
-			);
-			$this->facebook=new facebook($config);
+			// $this->secret_key="f57c5b006bae8c63bc170578bd582589";
+			// $this->app_id="436860186416914";
+			// $this->eid=$eid;
+			// $config=array(
+			// 	'appId' => $this->app_id,
+			//      'secret' => $this->secret_key,
+			//      'file_upload' => false,
+			//      'allowSignedRequest' => false,
+			// );
+			// $this->facebook=new facebook($config);
 	}
-	public function get_all_event_members() {
-			echo "uh oh";
-			$call=$this->facebook->api('/'.$this->eid.'/attending/');
-			$members=$call['data'];
-			$next=$call['data']['next'];
-			while(  $next != null ) {
-				$call=$this->facebook->api($next);
-				$members=array_merge( $members,$call['data']['next'] );
-			}
-			return $members;
-	}
+	// public function get_all_event_members() {
+	// 		echo "uh oh";
+	// 		$call=$this->facebook->api('/'.$this->eid.'/attending/');
+	// 		$members=$call['data'];
+	// 		$next=$call['data']['next'];
+	// 		while(  $next != null ) {
+	// 			$call=$this->facebook->api($next);
+	// 			$members=array_merge( $members,$call['data']['next'] );
+	// 		}
+	// 		return $members;
+	// }
 
 	// public function process_event_members($event_members) {
 	// 	foreach ($event_members as $key => $member) {

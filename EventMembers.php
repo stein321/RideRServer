a14025b73523586ad9cec240e3b4ddcdcc6f95a8
Ben Stein <stein321@gmail.com>
2014-02-23 09:45:23 -0800

    changed array access for 'next' parsing

--- EventMembers.php
+++ EventMembers.php
@@ -9,8 +9,8 @@ class EventMembers{
 	private $app_id;
 
 	public function __construct() {
-			// $this->secret_key="f57c5b006bae8c63bc170578bd582589";
-			// $this->app_id="436860186416914";
+			$this->secret_key="f57c5b006bae8c63bc170578bd582589";
+			$this->app_id="436860186416914";
 			// $this->eid=$eid;
 			// $config=array(
 			// 	'appId' => $this->app_id,

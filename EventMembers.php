f8aa496b3dc7966af9f523fe30818f3fe587239e
Ben Stein <stein321@gmail.com>
2014-02-23 10:22:43 -0800

    should be stable. return 1000 results

--- EventMembers.php
+++ EventMembers.php
@@ -21,7 +21,6 @@ class EventMembers{
 			$this->facebook=new facebook($config);
 	}
 	public function get_all_event_members() {
-			echo "uh oh";
 			$call=$this->facebook->api('/'.$this->eid.'/attending/');
 			$members=$call['data'];
 			// $next=$call['paging']['next'];

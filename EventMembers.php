ac0ae5177607fc4905c0dd2c6f97fef0319412ed
Ben Stein <stein321@gmail.com>
2014-02-23 12:25:12 -0800

    changed array syntax

--- EventMembers.php
+++ EventMembers.php
@@ -9,7 +9,6 @@ class EventMembers{
 	private $app_id;
 
 	public function __construct($event_id) {
-		echo "here";
 			$this->secret_key="f57c5b006bae8c63bc170578bd582589";
 			$this->app_id="436860186416914";
 			$this->eid=$event_id;

397af0ae7a4e0ab989abbaf626e28741341821fc
Ben Stein <stein321@gmail.com>
2014-02-23 10:02:24 -0800

    changed array access for 'next' parsing

--- EventMembers.php
+++ EventMembers.php
@@ -8,17 +8,17 @@ class EventMembers{
 	// private $secret_key;
 	// private $app_id;
 
-	public function __construct() {
-			// $this->secret_key="f57c5b006bae8c63bc170578bd582589";
-			// $this->app_id="436860186416914";
-			// $this->eid=$eid;
-			// $config=array(
-			// 	'appId' => $this->app_id,
-			//      'secret' => $this->secret_key,
-			//      'file_upload' => false,
-			//      'allowSignedRequest' => false,
-			// );
-			// $this->facebook=new facebook($config);
+	public function __construct($eid) {
+			$this->secret_key="f57c5b006bae8c63bc170578bd582589";
+			$this->app_id="436860186416914";
+			$this->eid=$eid;
+			$config=array(
+				'appId' => $this->app_id,
+			     'secret' => $this->secret_key,
+			     'file_upload' => false,
+			     'allowSignedRequest' => false,
+			);
+			$this->facebook=new facebook($config);
 	}
 	// public function get_all_event_members() {
 	// 		echo "uh oh";
--- index.php
+++ index.php
@@ -14,8 +14,8 @@ $facebook=new Get_Friends($id);
 // $friends=$facebook->get_friends();
 //var_dump($friends);
 
-$event=new EventMembers();
-// echo "boom2";
+$event=new EventMembers("1452625398292441");
+echo "boom2";
 // $members=$event->get_all_event_members();
 // var_dump($members);
 

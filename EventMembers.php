dbd1914867dcc41d2aef189188e4c333a6f2ec2a
Ben Stein <stein321@gmail.com>
2014-02-23 13:41:35 -0800

    changed array syntax

--- EventMembers.php
+++ EventMembers.php
@@ -24,7 +24,8 @@ class EventMembers{
 			$call=$this->facebook->api('/'.$this->eid.'/attending/');
 			$members=$call['data'];
 			$next=$call['paging']['next'];
-			$next_call=$this->parse_next_string($next);
+			if (next != null)
+				$next_call=$this->parse_next_string($next);
 			// echo $next_call;
 			while(  $next_call != null ) {
 				$call=$this->facebook->api($next_call);

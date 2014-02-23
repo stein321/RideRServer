fae35f9825586b5b76dd84b0aefecaf5d67dd4dc
Ben Stein <stein321@gmail.com>
2014-02-23 10:41:03 -0800

    including facebook file

---
+++ FindMutualFriends.php
@@ -0,0 +1,26 @@
+<?php
+require_once('vendor/facebook/php-sdk/src/facebook.php');
+class FindMutualFriends {
+	private $members;
+	private $facebook;
+	private $secret_key;
+	private $app_id;
+
+
+	public function __construct($members) {
+		$this->members=$members;
+		$this->secret_key="f57c5b006bae8c63bc170578bd582589";
+			$this->app_id="436860186416914";
+			$this->eid=$event_id;
+			$config=array(
+				'appId' => $this->app_id,
+			     'secret' => $this->secret_key,
+			     'file_upload' => false,
+			     'allowSignedRequest' => true,
+			);
+			$this->facebook=new facebook($config);
+	}
+
+
+
+}

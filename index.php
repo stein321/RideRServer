dabfb18fd7ead0486b4ea0368d39f824eb0997e8
Ben Stein <stein321@gmail.com>
2014-02-22 23:09:24 -0800

    nb

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 
 
-require_once('vendor/facebook/php-sdk/src/facebook.php');
-include_once('getFriends.php');
-// include_once('EventMembers.php');
-$secret="f57c5b006bae8c63bc170578bd582589";
-$app_id="436860186416914";
+// require_once('vendor/facebook/php-sdk/src/facebook.php');
+// include_once('getFriends.php');
+// // include_once('EventMembers.php');
+// $secret="f57c5b006bae8c63bc170578bd582589";
+// $app_id="436860186416914";
 
 var_dump($_GET);
 die();
-$id=$_GET['id'];
-$facebook=new Get_Friends($id);
-$friends=$facebook->get_friends();
-var_dump($friends);
+// $id=$_GET['id'];
+// $facebook=new Get_Friends($id);
+// $friends=$facebook->get_friends();
+// var_dump($friends);
 
 // $event=new EventMembers();
 //$members=$event->get_all_event_members();

976c29b2139459043c89719c2b85f715c89bb2c4
Ben Stein <stein321@gmail.com>
2014-02-22 23:35:17 -0800

    changed array access for 'next' parsing

--- EventMembers.php
+++ EventMembers.php
@@ -4,9 +4,11 @@ require_once('vendor/facebook/php-sdk/src/facebook.php');
 class EventMembers{
 	private $facebook;
 	private $eid;
+	private $secret_key;
+	private $app_id;
 
 	public function __construct($eid) {
-		$this->secret_key="f57c5b006bae8c63bc170578bd582589";
+		$this->setSecret_key("f57c5b006bae8c63bc170578bd582589");
 			$this->app_id="436860186416914";
 			$this->eid=$eid;
 			// $config=array(
@@ -35,4 +37,100 @@ class EventMembers{
 	// 		$call=$this->facebook->api('/'.$member['id']./'mutualfriends')
 	// 	}
 	// }
+
+    /**
+     * Gets the value of facebook.
+     *
+     * @return mixed
+     */
+    public function getFacebook()
+    {
+        return $this->facebook;
+    }
+    
+    /**
+     * Sets the value of facebook.
+     *
+     * @param mixed $facebook the facebook
+     *
+     * @return self
+     */
+    public function setFacebook($facebook)
+    {
+        $this->facebook = $facebook;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of eid.
+     *
+     * @return mixed
+     */
+    public function getEid()
+    {
+        return $this->eid;
+    }
+    
+    /**
+     * Sets the value of eid.
+     *
+     * @param mixed $eid the eid
+     *
+     * @return self
+     */
+    public function setEid($eid)
+    {
+        $this->eid = $eid;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of secret_key.
+     *
+     * @return mixed
+     */
+    public function getSecret_key()
+    {
+        return $this->secret_key;
+    }
+    
+    /**
+     * Sets the value of secret_key.
+     *
+     * @param mixed $secret_key the secret_key
+     *
+     * @return self
+     */
+    public function setSecret_key($secret_key)
+    {
+        $this->secret_key = $secret_key;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of app_id.
+     *
+     * @return mixed
+     */
+    public function getApp_id()
+    {
+        return $this->app_id;
+    }
+    
+    /**
+     * Sets the value of app_id.
+     *
+     * @param mixed $app_id the app_id
+     *
+     * @return self
+     */
+    public function setApp_id($app_id)
+    {
+        $this->app_id = $app_id;
+
+        return $this;
+    }
 }
